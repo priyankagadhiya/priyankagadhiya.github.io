0860ff0df8a827568fd5e5ae21f56e1eb6953268
priyankagadhiya28 <priyankagadhiya28@gmail.com>
2022-04-15 11:05:57 +0530

    send mail

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
-    $to =  $_POST['to'];
-    $subject = "";
-    $message = $_POST['message'];
-    $from = "priyankagadhiya28@gmail.com";
-    $headers = "From: $from";
+    // $to =  $_POST['to'];
+    // $subject = "";
+    // $message = $_POST['message'];
+    // $from = "priyankagadhiya28@gmail.com";
+    // $headers = "From: $from";
     echo "Hi";
-    return true;
-    mail($to, $subject, $message, $headers);
-    echo json_encode(["message" => "Thank you for reaching out to us! We will contact you very soon!"]);
+    // return true;
+    // mail($to, $subject, $message, $headers);
+    // echo json_encode(["message" => "Thank you for reaching out to us! We will contact you very soon!"]);
 ?>
