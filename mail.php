d10c9eb91910676d94cd06cb178af48448d46c7a
priyankagadhiya28 <priyankagadhiya28@gmail.com>
2022-04-15 11:00:11 +0530

    send mail

--- mail.php
+++ mail.php
@@ -4,6 +4,8 @@
     $message = $_POST['message'];
     $from = "priyankagadhiya28@gmail.com";
     $headers = "From: $from";
+    echo "Hi";
+    return true;
     mail($to, $subject, $message, $headers);
     echo json_encode(["message" => "Thank you for reaching out to us! We will contact you very soon!"]);
 ?>
