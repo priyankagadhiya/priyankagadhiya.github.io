cd9220c55c2c002f89b8275c8cdb5f9c86dd33f1
priyankagadhiya28 <priyankagadhiya28@gmail.com>
2022-04-17 09:40:15 +0530

    Attached google form

--- js/custom.js
+++ js/custom.js
@@ -60,58 +60,63 @@ $(document).ready(function () {
     // ===============================================================================
     //      Contact From Submit
     // ===============================================================================
-    $('#con_submit').click(function () {
-        var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    // $('#con_submit').click(function () {
+    //     var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-        var validation = true;
-        if($.trim($('#con_name').val()) == ""){
+    //     var validation = true;
+    //     if($.trim($('#con_name').val()) == ""){
 
-            validation = false;
-            $("#con_name_error").show();
-        }else{
-            $("#con_name_error").hide();
-        }
+    //         validation = false;
+    //         $("#con_name_error").show();
+    //     }else{
+    //         $("#con_name_error").hide();
+    //     }
 
-        if($.trim($('#con_email').val()) == ""){
+    //     if($.trim($('#con_email').val()) == ""){
 
-            validation = false;
-            $("#con_blank_email_error").show();
-        }else{
-            $("#con_blank_email_error").hide();
+    //         validation = false;
+    //         $("#con_blank_email_error").show();
+    //     }else{
+    //         $("#con_blank_email_error").hide();
 
-            if(!emailRegex.test($.trim($('#con_email').val()))){
+    //         if(!emailRegex.test($.trim($('#con_email').val()))){
 
-                validation = false;
-                $("#con_email_error").show();
-            }else{
-                $("#con_email_error").hide();
-            }
-        }
+    //             validation = false;
+    //             $("#con_email_error").show();
+    //         }else{
+    //             $("#con_email_error").hide();
+    //         }
+    //     }
 
-        if($.trim($('#con_message').val()) == ""){
+    //     if($.trim($('#con_message').val()) == ""){
 
-            validation = false;
-            $("#con_message_error").show();
-        }else{
-            $("#con_message_error").hide();
-        }
+    //         validation = false;
+    //         $("#con_message_error").show();
+    //     }else{
+    //         $("#con_message_error").hide();
+    //     }
 
-        if(validation){
-             $.post('/mail.php', {
-                to: $('#con_email').val(), 
-                message: $('#con_message').val()
-            }, function(response){ 
-                $('#con_name').val("");
-                $('#con_email').val("");
-                $('#con_message').val("");
-                $("#success_msg").text(response.message);
+    //     if(validation) {
+    //          $.post('/mail.php', {
+    //             to: $('#con_email').val(), 
+    //             message: $('#con_message').val()
+    //         }, function(response){ 
+    //             $('#con_name').val("");
+    //             $('#con_email').val("");
+    //             $('#con_message').val("");
+    //             $("#success_msg").text(response.message);
 
-                setTimeout(function(){
-                    $("#success_msg").text();
-                }, 5000);
-            }, "json");            
-        }
+    //             setTimeout(function(){
+    //                 $("#success_msg").text();
+    //             }, 5000);
+    //         }, "json");            
+    //     }
         
+    // });
+    var submitted=false;
+    $('#gform').on('submit', function(e) {
+        $('#gform *').fadeOut(2000);
+        $('#gform').prepend('Your submission has been processed...');
     });
     
 });
