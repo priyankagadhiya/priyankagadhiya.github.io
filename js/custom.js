982d4addbbbb531ca8c9b2ceae4f80ab5b63c90c
priyankagadhiya28 <priyankagadhiya28@gmail.com>
2022-04-18 09:23:44 +0530

    set up contact us form

--- js/custom.js
+++ js/custom.js
@@ -60,64 +60,64 @@ $(document).ready(function () {
     // ===============================================================================
     //      Contact From Submit
     // ===============================================================================
-    // $('#con_submit').click(function () {
-    //     var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    $('#con_submit').click(function () {
+        var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-    //     var validation = true;
-    //     if($.trim($('#con_name').val()) == ""){
-
-    //         validation = false;
-    //         $("#con_name_error").show();
-    //     }else{
-    //         $("#con_name_error").hide();
-    //     }
-
-    //     if($.trim($('#con_email').val()) == ""){
-
-    //         validation = false;
-    //         $("#con_blank_email_error").show();
-    //     }else{
-    //         $("#con_blank_email_error").hide();
-
-    //         if(!emailRegex.test($.trim($('#con_email').val()))){
-
-    //             validation = false;
-    //             $("#con_email_error").show();
-    //         }else{
-    //             $("#con_email_error").hide();
-    //         }
-    //     }
-
-    //     if($.trim($('#con_message').val()) == ""){
-
-    //         validation = false;
-    //         $("#con_message_error").show();
-    //     }else{
-    //         $("#con_message_error").hide();
-    //     }
-
-    //     if(validation) {
-    //          $.post('/mail.php', {
-    //             to: $('#con_email').val(), 
-    //             message: $('#con_message').val()
-    //         }, function(response){ 
-    //             $('#con_name').val("");
-    //             $('#con_email').val("");
-    //             $('#con_message').val("");
-    //             $("#success_msg").text(response.message);
-
-    //             setTimeout(function(){
-    //                 $("#success_msg").text();
-    //             }, 5000);
-    //         }, "json");            
-    //     }
+        var validation = true;
         
-    // });
-    $('#gform').on('submit', function(e) {
-        $('#gform *').fadeOut(2000);
-        $('#gform').prepend('Your submission has been processed...');
+        //Name validation
+        if($.trim($('#con_name').val()) == ""){
+
+            validation = false;
+            $("#con_name_error").show();
+        }else{
+            $("#con_name_error").hide();
+        }
+
+        //Email validation
+        if($.trim($('#con_email').val()) == ""){
+
+            validation = false;
+            $("#con_blank_email_error").show();
+        }else{
+            $("#con_blank_email_error").hide();
+
+            if(!emailRegex.test($.trim($('#email').val()))){
+
+                validation = false;
+                $("#con_email_error").show();
+            }else{
+                $("#con_email_error").hide();
+            }
+        }
+
+        //Message validation
+        if($.trim($('#con_message').val()) == ""){
+
+            validation = false;
+            $("#con_messe_error").show();
+        }else{
+            $("#con_message_error").hide();
+        }
+
+        if(validation) {
+            submitForm();
+        }
+        
     });
-    
+
+    async function submitForm() {
+        await $('form#gform').submit(); 
+        
+        $('#con_name').val("");
+        $('#con_email').val("");
+        $('#con_message').val("");
+        
+        $("#success_msg").text("Thank you for reaching out to us! We will contact you very soon!");
+        setTimeout(function(){
+            $("#success_msg").text("");
+        }, 5000);
+    }
 });
 
 
